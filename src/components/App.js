73189fed29f0cd20d96e091e21a2cab679d451a2
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-11 09:20:39 -0700

    Add component to app.js, add cidercontrol component with initial state and render if/else statements.

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,12 @@
 import React from 'react';
 
+
+function App() {
+  return (
+    <React.Fragment>
+      <CiderControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/CiderControl.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class CiderControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainCiderList: [],
+      selectedCider: null
+    };
+  }
+
+  render () {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if(this.state.selectedCider != null) { //if a specific cider has been selected
+      currentlyVisibleState = <CiderDetail cider = { this.state.selectedCider } />
+      buttonText = "Return to Cider Menu";
+    } else if (this.state.formVisibleOnPage) { //if someone clicked the add a cider button
+      currentlyVisibleState = <AddCider onNewCiderTapped = { this.handleAddingNewCiderToMenu } />
+      buttonText = "Return to Cider Menu";
+    } else { //if no cider selected and not viewing form to add a cider
+      currentlyVisibleState = <CiderMenu ciderMenu = { this.state.mainCiderList } onCiderSelection = { this.handleChangingSelectedCider } />
+      buttonText = "Add Cider";
+    }
+
+    return (
+      <React.Fragment>
+        { currentlyVisibleState }
+    <button onClick = { this.handleClick }>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CiderControl;
