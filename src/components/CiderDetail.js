618c66909583877ed5e0af4828405c1c706fc214
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-11 14:15:13 -0700

    Update ciderdetails component.

--- src/components/CiderDetail.js
+++ src/components/CiderDetail.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Cider from './Cider';
+import PropTypes from 'prop-types';
+
 
 function CiderDetail(props) {
+  const { cider } = props; //using object desctructuring to grab cider object from props
+
   return (
     <React.Fragment>
-      <h1>Cider Details</h1>
+      <h1>Cider Details: {cider.name}</h1>
+      <div>
+        <h3><em>{cider.brewery}</em></h3>
+        <p>ABV: {cider.alcoholContent}% | Price: ${cider.price}.00</p>
+      </div>
+      {/* add button to decrement here? */}
     </React.Fragment>
   );
 }
+
+CiderDetail.propTypes = {
+  cider: PropTypes.object
+};
 
 export default CiderDetail;
