907fd2d762e1807ffddb67936d02c6c3dc4f13f1
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-11 11:12:13 -0700

    Add addciderform component.

---
+++ src/components/AddCiderForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+
+function AddCiderForm(props) {
+
+  function handleAddCiderFormSubmission(event) {
+    event.preventDefault();
+    props.onNewCiderTapped({ name: event.target.name.value, brewery: event.target.brewery.value, alcoholContent: event.target.alcoholContent.value, price: event.target.price.value, remainingPints: event.target.remainingPints.value, id: v4() });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler = { handleAddCiderFormSubmission } //in ticket ex this was handleNewTicketFormSub
+        buttonText = "Add to Menu" />
+    </React.Fragment>
+  );
+}
+
+AddCiderForm.propTypes = {
+  onNewCiderTapped: PropTypes.func
+};
+
+export default AddCiderForm;
