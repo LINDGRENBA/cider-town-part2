fe94d3647311da15abf0de821c91cab7e74f7956
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-11 12:02:35 -0700

    Add ciderdetail component with placeholder.

---
+++ src/components/CiderDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cider from './Cider';
+
+function CiderDetail(props) {
+  return (
+    <React.Fragment>
+      <h1>Cider Details</h1>
+    </React.Fragment>
+  );
+}
+
+export default CiderDetail;
