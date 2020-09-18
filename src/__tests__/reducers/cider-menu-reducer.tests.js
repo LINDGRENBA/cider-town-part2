d6bb670d49a8fa241f22c12c2cc360e8ecdff570
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-18 08:36:07 -0700

    Append .js to cider-menu-reducer and correct describe spelling to achieve meaningful fail on first test.

--- src/__tests__/reducers/cider-menu-reducer.tests.js
+++ src/__tests__/reducers/cider-menu-reducer.tests.js
@@ -1,6 +1,6 @@
 import ciderMenuReducer from '../../reducers/cider-menu-reducer';
 
-discribe('ciderMenuReducer', () => {
+describe('ciderMenuReducer', () => {
 
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ciderMenuReducer({}, { type: null })).toEqual({});
