ffff2b8e87802f471c1057a3857fb41767af6994
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-18 10:01:28 -0700

    Fail second cidermenu test.

--- src/__tests__/reducers/cider-menu-reducer.tests.js
+++ src/__tests__/reducers/cider-menu-reducer.tests.js
@@ -2,10 +2,33 @@ import ciderMenuReducer from '../../reducers/cider-menu-reducer';
 
 describe('ciderMenuReducer', () => {
 
+  let action;
+  let ciderData = {
+    type: 'ADD_TICKET',
+    name:"Blackberry Cider",
+    brewery:"Avid",
+    alcoholContent: 6.2,
+    price: 6,
+    remainingPints: 124,
+    id: 1
+  }
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ciderMenuReducer({}, { type: null })).toEqual({});
   });
 
-  
+  test('Should successfully add a new cider object to the menu', () => {
+    expect(ciderMenuReducer({}, action)).toEqual({
+      [id] : {
+        type: 'ADD_TICKET',
+        name:"Blackberry Cider",
+        brewery:"Avid",
+        alcoholContent: 6.2,
+        price: 6,
+        remainingPints: 124,
+        id: 1
+      }
+    });
+  });
 
 });
--- src/reducers/cider-menu-reducer.js
+++ src/reducers/cider-menu-reducer.js
@@ -1,3 +1,18 @@
 export default(state = {}, action) => {
+  const { name, brewery, alcoholContent, price, remainingPints, id } = action;
+  switch(action.type) {
+    case 'ADD_TICKET':
+      return Object.assign({}, state, {
+        [id] : {
+          type: 'ADD_TICKET',
+          name:"Blackberry Cider",
+          brewery:"Avid",
+          alcoholContent: 6.2,
+          price: 6,
+          remainingPints: 124,
+          id: 1
+        }
+      });
+  }
   return state;
 };
